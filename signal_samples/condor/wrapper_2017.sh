3d57b9026b4966da0eaebdf53a9f3d16137a0ee6
zhaoyang <yuanzhaoyang658@outlook.com>
2021-05-20 23:42:57 +0800

    ok

--- signal_samples/condor/wrapper_2017.sh
+++ signal_samples/condor/wrapper_2017.sh
@@ -21,28 +21,28 @@ cmsRun SMP-RunIISummer20UL17SIM-00014_1_cfg.py
 # digipremix
 echo "____ start premix ____"
 cmssw_setup sandbox-CMSSW_10_6_17_patch1-aa4ec67.tar.gz
-cmsRun SMP-RunIISummer20UL16DIGIPremix-00018_1_cfg.py 
+cmsRun SMP-RunIISummer20UL17DIGIPremix-00014_1_cfg.py
 
 # HLT
 echo "____ start HLT ____"
-cmssw_setup sandbox-CMSSW_8_0_33_UL-0e2eb95.tar.gz
-cmsRun SMP-RunIISummer20UL16HLT-00021_1_cfg.py 
+cmssw_setup sandbox-CMSSW_9_4_14_UL_patch1-f007dfc.tar.gz
+cmsRun SMP-RunIISummer20UL17HLT-00013_1_cfg.py
 
 
 # reco
 echo "____ start reco ____"
 cmssw_setup sandbox-CMSSW_10_6_17_patch1-aa4ec67.tar.gz
-cmsRun SMP-RunIISummer20UL16RECO-00021_1_cfg.py 
+cmsRun SMP-RunIISummer20UL17RECO-00013_1_cfg.py
 
 # miniAod
 echo "____ start miniAod ____"
 cmssw_setup sandbox-CMSSW_10_6_17_patch1-aa4ec67.tar.gz
-cmsRun SMP-RunIISummer20UL16MiniAOD-00021_1_cfg.py 
+cmsRun SMP-RunIISummer20UL17MiniAOD-00013_1_cfg.py
 
 # NanoAod
 echo "____ start NanoAod ____"
 cmssw_setup sandbox-CMSSW_10_6_19_patch2-0147398.tar.gz
-cmsRun SMP-RunIISummer20UL16NanoAODv2-00047_1_cfg.py 
+cmsRun SMP-RunIISummer20UL17NanoAODv2-00036_1_cfg.py
 echo "_____________________"
 pwd
 ls -lvh
--- signal_samples/condor/wrapper_2018.sh
+++ signal_samples/condor/wrapper_2018.sh
@@ -8,41 +8,41 @@ echo "____ start lhegen ____"
 cmssw_setup sandbox-CMSSW_10_6_17_patch1-aa4ec67.tar.gz
 
 SEED=$(($(date +%s) % 100 + 1)) && echo $SEED
-sed -i "s/process.RandomNumberGeneratorService.externalLHEProducer.initialSeed=int.*$/process.RandomNumberGeneratorService.externalLHEProducer.initialSeed=int(${SEED})/" SMP-RunIISummer20UL16wmLHEGEN-00004_1_cfg.py 
-cmsRun  SMP-RunIISummer20UL16wmLHEGEN-00004_1_cfg.py 
+sed -i "s/process.RandomNumberGeneratorService.externalLHEProducer.initialSeed=int.*$/process.RandomNumberGeneratorService.externalLHEProducer.initialSeed=int(${SEED})/" SMP-RunIISummer20UL18wmLHEGEN-00002_1_cfg.py 
+cmsRun  SMP-RunIISummer20UL18wmLHEGEN-00002_1_cfg.py 
 
 
 # Sim
 echo "____ start Sim ____"
 cmssw_setup sandbox-CMSSW_10_6_17_patch1-aa4ec67.tar.gz
-cmsRun SMP-RunIISummer20UL16SIM-00021_1_cfg.py 
+cmsRun SMP-RunIISummer20UL18SIM-00002_1_cfg.py 
 
 
 # digipremix
 echo "____ start premix ____"
 cmssw_setup sandbox-CMSSW_10_6_17_patch1-aa4ec67.tar.gz
-cmsRun SMP-RunIISummer20UL16DIGIPremix-00018_1_cfg.py 
+cmsRun SMP-RunIISummer20UL18DIGIPremix-00002_1_cfg.py
 
 # HLT
 echo "____ start HLT ____"
-cmssw_setup sandbox-CMSSW_8_0_33_UL-0e2eb95.tar.gz
-cmsRun SMP-RunIISummer20UL16HLT-00021_1_cfg.py 
+cmssw_setup sandbox-CMSSW_10_2_16_UL-bc46fcf.tar.gz
+cmsRun SMP-RunIISummer20UL18HLT-00002_1_cfg.py
 
 
 # reco
 echo "____ start reco ____"
 cmssw_setup sandbox-CMSSW_10_6_17_patch1-aa4ec67.tar.gz
-cmsRun SMP-RunIISummer20UL16RECO-00021_1_cfg.py 
+cmsRun SMP-RunIISummer20UL18RECO-00002_1_cfg.py
 
 # miniAod
 echo "____ start miniAod ____"
 cmssw_setup sandbox-CMSSW_10_6_17_patch1-aa4ec67.tar.gz
-cmsRun SMP-RunIISummer20UL16MiniAOD-00021_1_cfg.py 
+cmsRun SMP-RunIISummer20UL18MiniAOD-00002_1_cfg.py
 
 # NanoAod
 echo "____ start NanoAod ____"
 cmssw_setup sandbox-CMSSW_10_6_19_patch2-0147398.tar.gz
-cmsRun SMP-RunIISummer20UL16NanoAODv2-00047_1_cfg.py 
+cmsRun  SMP-RunIISummer20UL18NanoAODv2-00019_1_cfg.py
 echo "_____________________"
 pwd
 ls -lvh
