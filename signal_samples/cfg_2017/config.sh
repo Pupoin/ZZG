fa0d13336d9a7f4aaed5ed3599d046d414dbb5b8
zhaoyang <yuanzhaoyang658@outlook.com>
2021-05-20 23:06:00 +0800

    2017

--- signal_samples/cfg_2017/config.sh
+++ signal_samples/cfg_2017/config.sh
@@ -26,9 +26,9 @@ scram b
 cd ../..
 cmsDriver.py Configuration/GenProduction/python/SMP-RunIISummer20UL17wmLHEGEN-00001-fragment.py \
   --python_filename SMP-RunIISummer20UL17wmLHEGEN-00001_1_cfg.py --eventcontent RAWSIM,LHE \
-  --customise Configuration/DataProcessing/Utils.addMonitoring \
-  --datatier GEN,LHE --fileout file:SMP-RunIISummer20UL17wmLHEGEN-00001.root \
-  --conditions 106X_mc2017_realistic_v6 --beamspot Realistic25ns13TeVEarly2017Collision \
+  --customise Configuration/DataProcessing/Utils.addMonitoring --datatier GEN,LHE \
+  --fileout file:SMP-RunIISummer20UL17wmLHEGEN-00001.root --conditions 106X_mc2017_realistic_v6 \
+  --beamspot Realistic25ns13TeVEarly2017Collision \
   --customise_commands process.source.numberEventsInLuminosityBlock="cms.untracked.uint32(100)" \
   --customise_commands process.RandomNumberGeneratorService.externalLHEProducer.initialSeed="int(${SEED})" \
   --step LHE,GEN --geometry DB:Extended --era Run2_2017 --no_exec --mc -n $EVENTS || exit $? ;
